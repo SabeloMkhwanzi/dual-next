00399f465a5ab66a518d04b75c88aec4c37a92fe
SabeloMkhwanzi <sabelo.mailme@gmail.com>
2023-06-17 15:14:26 +0200

    fixed route url path

--- pages/gamersNftCollection/[contract].js
+++ pages/gamersNftCollection/[contract].js
@@ -1,6 +1,6 @@
 import React from "react";
 import Head from "next/head";
-import { NavbarHead, Footer, CollectionPage } from "../components";
+import { NavbarHead, Footer, CollectionPage } from "../../components";
 import { AppShell, ScrollArea } from "@mantine/core";
 
 export default function gamersNftCollection() {
